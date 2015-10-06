0d3ebe4aba9f8c549e1617d706760cbf39220221
facelessuser <faceless.shop@gmail.com>
2015-10-06 00:41:26 -0600

    Update related license stuff in regards to extra

--- pymdownx/extra.py
+++ pymdownx/extra.py
@@ -11,7 +11,7 @@ but substitues a few extensions with PyMdown extensions:
 
 MIT license.
 
-Copyright (c) 2014 - 2015 Isaac Muse <isaacmuse@gmail.com>
+Copyright (c) 2015 Isaac Muse <isaacmuse@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 documentation files (the "Software"), to deal in the Software without restriction, including without limitation
