2fceb11d0a94aa2240397024f8e40804d6e83a49
facelessuser <faceless.shop@gmail.com>
2015-12-28 20:52:50 -0700

    Add Py3.5 info to setup

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: Filters',
