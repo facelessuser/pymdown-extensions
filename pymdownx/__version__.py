f1c2c59034eae40fe3acdb462933a5c1afecde72
facelessuser <faceless.shop@gmail.com>
2017-01-01 15:07:35 -0700

    Bump version for release

--- pymdownx/__version__.py
+++ pymdownx/__version__.py
@@ -1,7 +1,7 @@
 """Version."""
 
 #   (major, minor, micro, release type, pre-release build, post-release build)
-version_info = (1, 2, 0, 'final', 0, 0)
+version_info = (1, 3, 0, 'final', 0, 0)
 
 
 def _version():
