c7f09039695d3b17f52915fd1f811baedb01fb20
facelessuser <faceless.shop@gmail.com>
2017-05-24 21:48:55 -0600

    Tweak stylings and fallback
    - Don’t add and remove `no-details`.
    - Don’t mess with outlines at this point.

--- docs/src/js/spoilers.js
+++ docs/src/js/spoilers.js
@@ -2,20 +2,24 @@
  * Converts details/summary tags into working elements in browsers that don't yet support them.
  * @return {void}
  */
-export default function() {
-  // https://mathiasbynens.be/notes/html5-details-jquery#comment-35
-  // Detect if details is supported in the browser
-  const isDetailsSupported = (function(doc) {
-    const el = doc.createElement("details")
+export default () => {
+
+  const isDetailsSupported = () => {
+    // https://mathiasbynens.be/notes/html5-details-jquery#comment-35
+    // Detect if details is supported in the browser
+    const el = document.createElement("details")
     let fake = false
+
     if (!("open" in el)) {
       return false
     }
-    const root = doc.body || (function() {
-      const de = doc.documentElement
+
+    const root = document.body || (() => {
+      const de = document.documentElement
       fake = true
-      return de.insertBefore(doc.createElement("body"), de.firstElementChild || de.firstChild)
+      return de.insertBefore(document.createElement("body"), de.firstElementChild || de.firstChild)
     })()
+
     el.innerHTML = "<summary>a</summary>b"
     el.style.display = "block"
     root.appendChild(el)
@@ -23,32 +27,44 @@ export default function() {
     el.open = true
     diff = diff !== el.offsetHeight
     root.removeChild(el)
+
     if (fake) {
       root.parentNode.removeChild(root)
     }
+
     return diff
-  })(document)
+  }
+
+  /* Toggle details state */
+  const toggle = e => {
+    const key = e.which
+    if (!key || key === 13 || key === 32) {
+      const details = e.target.parentNode
+      if (details.hasAttribute("open")) {
+        details.setAttribute("open", "open")
+      } else {
+        details.removeAttribute("open")
+      }
+      return false
+    }
+  }
 
   if (!isDetailsSupported) {
     const blocks = document.querySelectorAll("details>summary")
     for (let i = 0; i < blocks.length; i++) {
       const summary = blocks[i]
       const details = summary.parentNode
-      if (!details.hasAttribute("open")) {
-        if (!details.className.match(new RegExp("(\\s|^)no-details(\\s|$)"))) {
-          details.className += " no-details"
-        }
+
+      // Apply "no-details" to for unsupported details tags
+      if (!details.className.match(new RegExp("(\\s|^)no-details(\\s|$)"))) {
+        details.className += " no-details"
       }
+
       summary.addEventListener("click", e => {
-        const de = e.target.parentNode
-        if (de.className.match(new RegExp("(\\s|^)no-details(\\s|$)"))) {
-          const reg = new RegExp("(\\s|^)no-details(\\s|$)")
-          de.className = de.className.replace(reg, " ")
-          de.setAttribute("open", "")
-        } else {
-          de.className += " no-details"
-          de.removeAttribute("open")
-        }
+        toggle(e)
+      })
+      summary.addEventListener("keydown", e => {
+        toggle(e)
       })
     }
   }
