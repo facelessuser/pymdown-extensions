0916dc09210d62abe2fabd8ebdd91449daa6d35c
facelessuser <faceless.shop@gmail.com>
2018-08-02 10:01:51 -0600

    Test that the validator works

---
+++ tests/test_extensions/test_superfences.py
@@ -0,0 +1,49 @@
+"""Test cases for SuperFences."""
+from __future__ import unicode_literals
+from .. import util
+from pymdownx import util as pymd_util
+
+
+class TestSuperFences(util.MdCase):
+    """Test Details."""
+
+    extension = ['pymdownx.superfences']
+    extension_configs = {}
+
+    def test_bad_options(self):
+        """Test bad options."""
+
+        self.check_markdown(
+            r'''
+            ```python option="bad"
+            import test
+            ```
+            ''',
+            r'''
+            <p><code>python option="bad"
+            import test</code></p>
+            ''',
+            True
+        )
+
+    def test_bad_option_value(self):
+        """Test bad option values."""
+
+        self.check_markdown(
+            r'''
+            ```python hl_lines="unexpected 3" linenums="1"
+            """Some file."""
+            import foo.bar
+            import boo.baz
+            import foo.bar.baz
+            ```
+            ''',
+            r'''
+            <p><code>python hl_lines="unexpected 3" linenums="1"
+            """Some file."""
+            import foo.bar
+            import boo.baz
+            import foo.bar.baz</code></p>
+            ''',
+            True
+        )
