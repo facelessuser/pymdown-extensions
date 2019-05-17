398db7b8bf12e46b3f98c2ff01b17fbd2497134b
Isaac Muse <faceless.shop@gmail.com>
2019-05-17 06:44:04 -0600

    Don't use imp (#569)
    * Don't use imp
    
    * Use importlib

--- setup.py
+++ setup.py
@@ -4,19 +4,30 @@
 
 from setuptools import setup, find_packages
 import os
-import imp
+import sys
+
+PY2 = sys.version_info < (3, 0)
 
 
 def get_version():
     """Get version and version_info without importing the entire module."""
 
-    path = os.path.join(os.path.dirname(__file__), 'pymdownx')
-    fp, pathname, desc = imp.find_module('__meta__', [path])
-    try:
+    if PY2:
+        import imp
+
+        path = os.path.join(os.path.dirname(__file__), 'pymdownx')
+        fp, pathname, desc = imp.find_module('__meta__', [path])
         vi = imp.load_module('__meta__', fp, pathname, desc).__version_info__
-        return vi._get_canonical(), vi._get_dev_status()
-    finally:
         fp.close()
+    else:
+        import importlib.util
+
+        path = os.path.join(os.path.dirname(__file__), 'pymdownx', '__meta__.py')
+        spec = importlib.util.spec_from_file_location("__meta__", path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        vi = module.__version_info__
+    return vi._get_canonical(), vi._get_dev_status()
 
 
 def get_requirements(req):
@@ -50,6 +61,7 @@ setup(
     long_description_content_type='text/markdown',
     author='Isaac Muse',
     author_email='Isaac.Muse@gmail.com',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     url='https://github.com/facelessuser/pymdown-extensions',
     packages=find_packages(exclude=['tools', 'test*']),
     install_requires=get_requirements("requirements/project.txt"),
@@ -63,10 +75,9 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: Filters',
