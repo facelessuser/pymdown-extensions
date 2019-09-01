cad91961429d4f56322e48a48a9abe8c2a213e65
Isaac Muse <faceless.shop@gmail.com>
2019-08-31 20:13:06 -0600

    Prep for 6.1.0 (#653)
    Update changelog and version
    Remove lint workaround

--- pymdownx/__meta__.py
+++ pymdownx/__meta__.py
@@ -186,5 +186,5 @@ def parse_version(ver, pre=False):
     return Version(major, minor, micro, release, pre, post, dev)
 
 
-__version_info__ = Version(6, 1, 0, ".dev")
+__version_info__ = Version(6, 1, 0, "final")
 __version__ = __version_info__._get_canonical()
