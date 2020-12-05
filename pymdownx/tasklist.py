35c49438eb8b3742862c56b14a4d5b977a51cdaa
Isaac Muse <faceless.shop@gmail.com>
2020-12-05 16:32:14 -0700

    Set a lable of `task` to task list items (#1167)

--- pymdownx/tasklist.py
+++ pymdownx/tasklist.py
@@ -36,12 +36,12 @@ def get_checkbox(state, custom_checkbox=False, clickable_checkbox=False):
     if custom_checkbox:
         return (
             '<label class="task-list-control">' +
-            '<input type="checkbox"%s%s/>' % (
+            '<input type="checkbox"%s%s label="task"/>' % (
                 ' disabled' if not clickable_checkbox else '',
                 ' checked' if state.lower() == 'x' else '') +
             '<span class="task-list-indicator"></span></label> '
         )
-    return '<input type="checkbox"%s%s/> ' % (
+    return '<input type="checkbox"%s%s label="task"/> ' % (
         ' disabled' if not clickable_checkbox else '',
         ' checked' if state.lower() == 'x' else '')
 
