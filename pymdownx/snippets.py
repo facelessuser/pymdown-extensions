6af15b510710d382e1cfb962c8c24f921a6ac4f2
Simon Ungar Felding <sife@adm.ku.dk>
2024-11-14 16:43:14 +0100

    make the error message handle plural nicely

--- pymdownx/snippets.py
+++ pymdownx/snippets.py
@@ -122,7 +122,7 @@ class SnippetPreprocessor(Preprocessor):
                     new_lines.append(m[0])
             
         if not new_lines and self.check_paths:
-            flagstring = f"with flags {self.regex_flags}" if flags else "" # If flags is 0, we don't want to print it (re.NOFLAG == 0).
+            flagstring = f"with flag{'s' if len(self.regex_flags) > 1 else ''} {self.regex_flags}" if flags else "" # If flags is 0, we don't want to print it (re.NOFLAG == 0).
             raise SnippetMissingError(f"No line matched the regex /{regex}/ {flagstring}")
         
         return self.dedent(new_lines) if self.dedent_subsections else new_lines
