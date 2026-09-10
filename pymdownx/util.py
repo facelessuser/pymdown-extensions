55da51f649e6c2fdd4ac189c08a7e9dcddc97f73
facelessuser <faceless.shop@gmail.com>
2026-09-10 09:43:12 -0600

    Separate cache info setup vs reset

--- pymdownx/util.py
+++ pymdownx/util.py
@@ -217,19 +217,24 @@ class DelimiterProcessor(InlineProcessor):
 
         """
 
+        self.regions: list[tuple[int, int, int, int, int]] = []
+        self.stack: deque[tuple[int, int, bool, int]] = deque()
+        self.cache_index = 0
+        self.cache_pos = 0
+        self.cache_legacy_pos = -1
+
         self.no_space = no_space
         self.smart = smart
         self.tags = tags.split(',')
         self.double = len(tags) != 2 and double
-        self.reset()
         super().__init__(self._build_patterns(token), md)
 
     def reset(self) -> None:
         """Reset."""
 
         # Cache info
-        self.regions: list[tuple[int, int, int, int, int]] = []
-        self.stack: deque[tuple[int, int, bool, int]] = deque()
+        self.regions.clear()
+        self.stack.clear()
         self.cache_index = 0
         self.cache_pos = 0
         self.cache_legacy_pos = -1
